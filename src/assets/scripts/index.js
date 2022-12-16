da860fb5ada7eac30664029a9fb35c7d19d31ede
Francis Rubio <96061229+teacherbuknoy@users.noreply.github.com>
2022-12-16 11:07:40 +0800

    Enable color scheme picker

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,1 +1,4 @@
 import "./animations/navigation-bar"
+import { ColorScheme } from "./libraries/color-scheme"
+
+window.colorScheme = new ColorScheme();
---
+++ src/assets/scripts/libraries/color-scheme.js
@@ -0,0 +1,45 @@
+class ColorScheme {
+  constructor() {
+    const form = document.forms['color-scheme']
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      this.#setColorScheme(form['color-scheme'].value)
+    })
+
+    const scheme = this.#loadFromLocalStorage()
+    this.scheme = ""
+    this.#setColorScheme(scheme)
+
+    form['color-scheme'].value = scheme
+  }
+
+  #saveToLocalStorage() {
+    if (window.localStorage) {
+      localStorage.setItem('color-scheme', this.scheme)
+    }
+  }
+
+  #loadFromLocalStorage() {
+    const colorScheme = localStorage.getItem('color-scheme')
+
+    return colorScheme ? colorScheme : "default"
+  }
+
+  /**
+   * Set the color scheme
+   * @param {"default" | "light" | "dark"} colorScheme the color scheme for this page
+   */
+  #setColorScheme(colorScheme) {
+    const body = document.body
+    body.classList.remove('theme-default', 'theme-light', 'theme-dark')
+    body.classList.add('theme-' + colorScheme)
+    this.scheme = colorScheme
+    this.#saveToLocalStorage()
+  }
+
+  setLight() { this.#setColorScheme('light') }
+  setDefault() { this.#setColorScheme('default') }
+  setDark() { this.#setColorScheme('dark') }
+}
+
+export { ColorScheme }
